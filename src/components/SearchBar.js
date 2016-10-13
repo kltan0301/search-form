91205bd7050a7bd3383e35eaba44c505461eca11
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-10-13 13:46:45 +0800

    Retrieve content on input change

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: ''};
+  }
+  //function to trigger on value change of input box
+  onInputChange(search) {
+    this.setState({search});
+    this.props.onSearchChange(search);
+  }
+  //add searchbar into app
+  render() {
+    return (
+      <div>
+        <input placeholder="Type place here" onChange={e => this.onInputChange(e.target.value)}/>
+      </div>
+    )
+  }
+}
+//react's equivalent of module.exports
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+  handleSearchChange(searchTerm) {
+    console.log(searchTerm);
+  }
   render() {
     return (
-      <div class="searchBar">
-        <p>Search bar comes here</p>
+      <div className="searchBar">
+        <SearchBar onSearchChange={this.handleSearchChange}/>
       </div>
     );
   }
